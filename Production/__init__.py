7aaa711cd39db763180ba4f2f501d6e0affd5033
Aditya Lesmana <adityalesmana@Adityas-MacBook-Air.local>
2024-11-20 22:20:43 +0700

    [FIX] Updating development branch

--- Production/__init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-from Production.config.core import config, PACKAGE_ROOT
-
-logging.getLogger(config.app_config.package_name).addHandler(logging.NullHandler())
-with open(PACKAGE_ROOT / 'VERSION') as version_file:
-    __version__ = version_file.read().strip()
