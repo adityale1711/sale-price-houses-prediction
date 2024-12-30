a3b6f11e603320b20b1a34bc1380cfe3d1b6677c
Aditya Lesmana <a.lesmana1711@gmail.com>
2024-12-31 02:55:11 +0700

    [FEAT] Preparing for circleci configuration

---
+++ model-package/tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+from sale_price_house_prediction_model.config.core import config
+from sale_price_house_prediction_model.processing.data_manager import load_dataset
+
+
+@pytest.fixture()
+def sample_input_data():
+    return load_dataset(file_name=config.app_cnf.test_data_file)
